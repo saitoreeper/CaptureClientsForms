c9b2214b45517aa0ff592cc2452b7856cfbeb8b0
Dominic Joubert <saitoreeper@gmail.com>
2018-05-31 11:07:06 +0200

    Enitities project refrenched in web and dbcontext added

---
+++ ClientCapture/DataContexts/ClientsCaptureDb.cs
@@ -0,0 +1,22 @@
+﻿using ClientCapture.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ClientCapture.DataContexts
+{
+    
+        public class ClientsCaptureDb : DbContext
+        {
+            public ClientsCaptureDb()
+                : base("DefaultConnection")
+            {
+            }
+
+            // Momentum.Entities.Base
+            public DbSet<Client> Clients { get; set; }
+            public DbSet<Contact> Contacts { get; set; }
+    }
+}
