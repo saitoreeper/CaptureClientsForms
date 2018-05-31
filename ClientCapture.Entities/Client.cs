30b7aab3c8ee8f25445ae69e5c50ff3bdf8b04c4
Dominic Joubert <saitoreeper@gmail.com>
2018-05-31 10:38:29 +0200

    Database Models Created

---
+++ ClientCapture.Entities/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCapture.Entities
+{
+    public class Client
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public string Industry { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Email { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string ContactNumber { get; set; }
+        public string Address { get; set; }
+        public bool BeeStatus { get; set; }
+        public DateTime Created { get; set; }
+
+        public ICollection<Contact> Contacts { get; set; }
+    }
+}
---
+++ ClientCapture.Entities/Contact.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientCapture.Entities
+{
+    public class Contact
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        [Required]
+        [MaxLength(255)]
+        public string Email { get; set; }
+        [MaxLength(15)]
+        public string ContactNumber { get; set; }
+
+        [ForeignKey("Clients")]
+        public int ClientsId { get; set; }
+        public Client Client { get; set; }
+    }
+}
